a304ed356d0d66a76cc17df1611ee69407dc0903
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-03-19 18:18:24 +0000

    RT & S

--- addresses.py
+++ addresses.py
@@ -1,4 +1,5 @@
 BUILDING = {
+	"DAV": "Loughborough University Physics Department, Epinal Way, Loughborough LE11 3TU",
 	"EHB": "EHB, Margaret Keay Rd, Loughborough LE11 3TU",
 	"LDS": "Loughborough Design School, Loughborough University, Loughborough LE11 3TU",
 	"MHL": "Martin Hall, Epinal Way, Loughborough LE11 3TS",
@@ -6,14 +7,15 @@ BUILDING = {
 	"SCH": "Schofield Building, University Rd, Loughborough LE11 3TU",
 	"SMB": "Stewart Mason Building, Margaret Keay Rd, Loughborough LE11 3TU",
 	"WAV": "Wavy Top, Loughborough LE11 3TU",
-	"WPT": "West Park Teaching Hub, 2 Oakwood Dr, Loughborough LE11 3QF",
 	"WPL": "STEMLab, University Rd, Loughborough LE11 3TL",
-	"DAV": "Loughborough University Physics Department, Epinal Way, Loughborough LE11 3TU",
+	"WPT": "West Park Teaching Hub, 2 Oakwood Dr, Loughborough LE11 3QF",
 	"CC": "James France, Margaret Keay Rd, Loughborough LE11 3TW",
+	"RT": "Sir Frank Gibb, Frank Gibb (Sir) Building, Loughborough LE11 3UE",
 	"TW": "Wolfson School of Mechanical, Electrical and Manufacturing Engineering, Loughborough University, Wolfson Building, Ashby Rd, Loughborough LE11 3TU",
 	"N": "Haslegrave Building, University Rd, Loughborough LE11 3TP",
 	"U": "Brockington Building, Loughborough University, Margaret Keay Rd, Loughborough LE11 3TU",
 	"T": "Wolfson School of Mechanical, Electrical and Manufacturing Engineering, Loughborough University, Wolfson Building, Ashby Rd, Loughborough LE11 3TU",
+	"S": "S building, Loughborough LE11 3UE",
 }
 
 BUILDING_CODES = tuple(sorted(BUILDING.keys(), key=len, reverse=True))
