f0ed67fcb1e5ebb01d7e644942e04d12b63e4f17
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2025-01-09 13:39:16 +0000

    change diff as an update is always required

--- gregle/lu/diff.py
+++ gregle/lu/diff.py
@@ -26,8 +26,7 @@ def changes(a: Iterable[EventSchedule], b: Iterable[EventSchedule]) -> Iterator[
                 lhs = EventSchedule.combine(a_first, *(i[1] for i in a_rest), eid=a_first.id())
                 for e in a_rest:
                     yield ("delete", e[1])
-                if _is_diff(lhs, rhs):
-                    yield ("update", (lhs, rhs))
+                yield ("update", (lhs, rhs))
                 raise NotImplementedError
             case x:
                 raise ValueError(x)
